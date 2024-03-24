cb5735b9424a412642d2b78a5b2e2968711150de
jotavetech <ojoaogugel@gmail.com>
2024-03-24 15:49:08 -0300

    add roboto font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Roboto_Mono } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const font = Roboto_Mono({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Bases Challange",
@@ -16,7 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="dark">
-      <body className={inter.className}>{children}</body>
+      <body className={font.className}>{children}</body>
     </html>
   );
 }
