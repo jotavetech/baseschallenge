b944a43c2271a7c68e34f6f03009ec3564d64fd9
jotavetech <ojoaogugel@gmail.com>
2024-04-25 10:10:53 -0300

    feat: add pwa

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+import withPWA from "next-pwa";
+
+const nextConfig = {
+  ...withPWA({
+    dest: "public",
+    register: true,
+  }),
+};
 
 export default nextConfig;
