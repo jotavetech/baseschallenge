1b959bdd5ca0b48930e47cdde4854e49465a3ea3
hiroshimorowaka <guilhermecabral1204@gmail.com>
2024-05-02 07:48:09 -0300

    feat: add and configure next-intl

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 
+import createNextIntlPlugin from "next-intl/plugin";
 import withPWA from "next-pwa";
+
+const withNextIntl = createNextIntlPlugin();
 
 const nextConfig = {
   ...withPWA({
@@ -10,4 +13,4 @@ const nextConfig = {
   }),
 };
 
-export default nextConfig;
+export default withNextIntl(nextConfig);
