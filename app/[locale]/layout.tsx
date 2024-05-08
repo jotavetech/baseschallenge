e2de3597d2ca14b7a20ff2ebb16f1245882555a2
jotavetech <ojoaogugel@gmail.com>
2024-05-08 09:30:16 -0300

    ad test

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -3,44 +3,46 @@ import Footer from "../components/layout/footer";
 import { Analytics } from "@vercel/analytics/react";
 import { ThemeProvider } from "../components/providers/theme-provider";
 import { Roboto_Mono } from "next/font/google";
-import { NextIntlClientProvider, useMessages, useTranslations } from "next-intl";
-
-
+import {
+  NextIntlClientProvider,
+  useMessages,
+  useTranslations,
+} from "next-intl";
 
 const font = Roboto_Mono({ subsets: ["latin"] });
 
-
 export default function PagesLayout({
   children,
-  params
+  params,
 }: {
   children: React.ReactNode;
   params: {
     locale: string;
-  }
+  };
 }) {
-
-
   const messages = useMessages();
-
 
   return (
     <html lang={params.locale} suppressHydrationWarning>
-    <body className={font.className}>
-      <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
-      <main className="bg-app-bg dark:bg-app-bg-dark">
-      <NextIntlClientProvider locale={params.locale} messages={messages}>
-  
-            <Header />
-            {children}
-            <Footer />
-            
-        </NextIntlClientProvider>
-      </main>
+      <head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4086113605281721"
+          crossorigin="anonymous"
+        ></script>
+      </head>
+      <body className={font.className}>
+        <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
+          <main className="bg-app-bg dark:bg-app-bg-dark">
+            <NextIntlClientProvider locale={params.locale} messages={messages}>
+              <Header />
+              {children}
+              <Footer />
+            </NextIntlClientProvider>
+          </main>
           <Analytics />
-      </ThemeProvider>
-    </body>
-  </html>
- 
+        </ThemeProvider>
+      </body>
+    </html>
   );
 }
